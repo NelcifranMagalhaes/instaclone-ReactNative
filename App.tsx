ede41aac7b44d4300bf0044655377d2efc81e63d
nelcifran <nelcifranpires@gmail.com>
2019-08-29 10:33:44 -0300

    scrollview added

--- App.tsx
+++ App.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, Image, Dimensions, ScrollView} from 'react-native';
 
+const width = Dimensions.get('screen').width;
+const height = Dimensions.get('screen').height;
+
+const photo = [ {id: 1, user: 'Sasuke'}, 
+  {id: 2, user: 'Naruto'},
+  {id: 3, user: 'Deidara'}];
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <ScrollView style = {{marginTop: 20}}>
+      {photo.map(f => 
+        <View key={f.id}>
+          <Text>{f.user}</Text>
+          <Image source={require('./resources/img/sasuke.png')}
+          style={{width: width, height: height}}/>
+        </View>
+      )}
+    </ScrollView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
